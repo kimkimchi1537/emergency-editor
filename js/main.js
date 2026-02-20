736392e2c36c0efe9777d098d2cdbd4b1d9e8c9c
kimkimchi1537 <kimkimchi1537@gmail.com>
2026-02-20 16:20:31 +0900

    수정 완료

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import { EvacuationEditor } from './Editor.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("[LOG] js/main.js: DOMContentLoaded 이벤트 발생");
-    const editor = new EvacuationEditor();
-    window.editor = editor;
-    console.log("[LOG] js/main.js: Evacuation Editor 초기화 성공");
-});
